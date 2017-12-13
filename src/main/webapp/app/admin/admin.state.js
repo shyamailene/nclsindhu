84b61f146ced2afa733ddebf4558dfba2fd8ba92
Ailene <shyam.ailene@hpe.com>
2017-12-13 19:23:40 +0530

    Initial application generated by JHipster-4.12.0

---
+++ src/main/webapp/app/admin/admin.state.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('nclsindhuApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig ($stateProvider) {
+        $stateProvider.state('admin', {
+            abstract: true,
+            parent: 'app'
+        });
+    }
+})();
